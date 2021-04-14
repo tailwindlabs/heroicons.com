c0c93be61fdd437133dd1f2af7152bde32f86838
Lukas <69743171+quick007@users.noreply.github.com>
2021-04-13 22:14:16 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
+  mode: "jit",
   purge: ['src/**/*.js'],
   theme: {
     extend: {
